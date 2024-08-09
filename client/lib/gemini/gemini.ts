b613dbb6df2dc59b5d4622bae5f64a79e4e6c851
Github11200 <pateljinay@outlook.com>
2024-08-08 18:36:58 -0700

    added in a really simple way to use the gemini api

--- client/lib/gemini/gemini.ts
+++ client/lib/gemini/gemini.ts
@@ -1,3 +1,16 @@
-export function askPrompt(prompt: string) {
-  return prompt;
+"use server";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const generativeAI = new GoogleGenerativeAI(
+  process.env.GEMINI_API_KEY as string
+);
+
+const model = generativeAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+export default async function getAnswer(prompt: string) {
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
 }
