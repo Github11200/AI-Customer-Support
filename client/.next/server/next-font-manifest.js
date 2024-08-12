3b1ff1a702b2a339a3b6aec59921b3eb032cff9c
Github11200 <pateljinay@outlook.com>
2024-08-11 22:07:55 -0700

    changes

--- client/.next/server/next-font-manifest.js
+++ client/.next/server/next-font-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+=======
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/user/code/projects/AI-Customer-Support/client/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+>>>>>>> c01fbb3 (changes)
--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -91,6 +91,21 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
+<<<<<<< HEAD
+=======
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+>>>>>>> c01fbb3 (changes)
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	!function() {
 /******/ 		var getProto = Object.getPrototypeOf ? function(obj) { return Object.getPrototypeOf(obj); } : function(obj) { return obj.__proto__; };
@@ -180,7 +195,11 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
+<<<<<<< HEAD
 /******/ 		__webpack_require__.h = function() { return "58b4eab2700abb51"; }
+=======
+/******/ 		__webpack_require__.h = function() { return "baddbabc6ca4354f"; }
+>>>>>>> c01fbb3 (changes)
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
